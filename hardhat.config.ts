2bef4d4678ad03b4b2c9f614079feaba6b17ae74
gabrielfst30 <gabrielfst30@gmail.com>
2025-04-27 23:08:31 -0300

    feature: configurações iniciais

---
+++ hardhat.config.ts
@@ -0,0 +1,33 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "solidity-coverage";
+import "hardhat-watcher";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+  watcher: {
+    compilation: {
+      tasks: ["compile"],
+    },
+    test: {
+      tasks: [{ command: "test", params: { testFiles: ["{path}"] } }],
+      files: ["./test/**/*"],
+      verbose: true,
+    },
+  },
+};
+
+export default config;
