d852346fd4fc6e4251455669d1d6380f2b4148e0
gabriel.ritta <gabriel.ritta@defensoria.ba.def.br>
2025-04-30 17:21:57 -0300

    chore: implementando teste de donation

--- test/donation/donation.ts
+++ test/donation/donation.ts
@@ -1,16 +1,22 @@
+//para rodar o teste -> npx hardhat test
+//para rodar o teste de forma automático -> npx hardhat test && npx hardhat watch test
+
+//Garante que a blockchain de teste será reinicializada para um estado limpo antes de cada teste, economizando tempo.
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 //verificador de aprovação dos testes
 import { expect } from "chai";
+//lib de interação de contratos inteligentes
 import { ethers } from "hardhat";
+//conversor de BigNumber para Wei
 import { parseEther } from "ethers";
 
-//Nome do teste
+//Testando implementação do contrato e dando nome ao teste
 describe("Donation", () => {
   async function setupFixture() {
     //lista de carteiras do hardhat para teste
     const [owner, billy, jonh, alice] = await ethers.getSigners();
 
-    //Pegando meu contrato
+    //Gerando meu contrato
     const Donation = await ethers.getContractFactory("Donation");
 
     //Fazendo deploy do contrato para usar nos testes
@@ -27,16 +33,33 @@ describe("Donation", () => {
       alice,
     };
   }
-  describe("Donation Receive", () => {
-    it("should receive a donation", async () => {
-      //
-      const { donationContract } = await loadFixture(setupFixture);
-      const value = parseEther("0.01");
-      await donationContract.donate({ value });
-    });
-    it("should not receive a donation if value is 0", async () => {});
 
-    it("should return balance equal 0 if hasn't donations", async () => {});
-    it("should return balance equal 0.05", async () => {});
+  //Testando a doação
+  describe("Recebendo Donation", () => {
+    it("Deve receber uma doação", async () => {
+      //Iniciando contrato no estado limpo
+      const { donationContract } = await loadFixture(setupFixture);
+      //Formatando valor em Wei e simulando o envio de um valor
+      const value = parseEther("0.01");
+      //Enviando doação
+      await donationContract.donate({ value });
+      //Lendo o total de doação acumulada
+      const total = await donationContract.total();
+      //Comparando se o total é igual ao valor da doação enviada
+      expect(total).to.equal(value);
+    });
+
+    it("Valor do donation deve ser maior que 0", async () => {
+      //Iniciando contrato no estado limpo
+      const { donationContract } = await loadFixture(setupFixture);
+  
+      //Formatando valor em Wei e simulando o envio de 0
+      const value = parseEther("0");
+  
+      //Enviando doação e rejeitando se for menor ou igual que zero
+      await expect(donationContract.donate({ value })).to.rejectedWith(
+        "valor inválido"
+      );
+    });
   });
 });
