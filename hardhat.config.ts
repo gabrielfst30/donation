071645c35b5106c09b61b40346c77e288d707061
gabrielfst30 <gabrielfst30@gmail.com>
2025-04-30 01:10:06 -0300

    fix: ajustando version do solidity

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ import "hardhat-watcher";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.28",
+    version: "0.8.29",
     settings: {
       optimizer: {
         enabled: true,
